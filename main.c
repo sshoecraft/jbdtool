0612488f7087544eebc34859dd58d9d526ed64e3
root <root@sunny.home.net>
2021-01-08 01:22:07 -0600

    refresh

--- main.c
+++ main.c
@@ -155,20 +155,6 @@ struct jbd_params *_getp(char *label) {
 	return 0;
 }
 
-void serialization_example(void) {
-    JSON_Value *root_value = json_value_init_object();
-    JSON_Object *root_object = json_value_get_object(root_value);
-    char *serialized_string = NULL;
-    json_object_set_string(root_object, "name", "John Smith");
-    json_object_set_number(root_object, "age", 25);
-    json_object_dotset_string(root_object, "address.city", "Cupertino");
-    json_object_dotset_value(root_object, "contact.emails", json_parse_string("[\"email@example.com\",\"email2@example.com\"]"));
-    serialized_string = json_serialize_to_string_pretty(root_value);
-    puts(serialized_string);
-    json_free_serialized_string(serialized_string);
-    json_value_free(root_value);
-}
-
 void dint(char *label, char *format, int val) {
 	char temp[128];
 
